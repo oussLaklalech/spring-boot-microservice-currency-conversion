baebf2d65322470dbc426c01158dc8f6316e2bc6
Laklalech Oussama <olaklalech@SQLI756-8.SQLI>
2018-05-06 16:50:28 +0200

    Using Ribbon for Load Balancing

--- src/main/java/com/olc/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/olc/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
@@ -1,10 +1,12 @@
 package com.olc.springboot.microservice.example.currencyconversion.springbootmicroservicecurrencyconversion;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="forex-service", url="localhost:8000")
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public CurrencyConversionBean retrieveExchangeValue
